4fd2377b39799766cf17d0c5ff4833e45d669862
esraa33-95 <esraahedia33@gmail.com>
2024-08-20 16:18:50 +0200

    task 13

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,10 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Support\Facades\Route;
+use App\Http\Middleware\IsAdmin;
+
+
 //use App\Http\Middleware\EnsureTokenIsValid;
 
 return Application::configure(basePath: dirname(__DIR__))
@@ -14,12 +18,12 @@ return Application::configure(basePath: dirname(__DIR__))
         //optional custom routefile
         then: function () 
         {
-            Route::prefix('teacher')->group(base_path('routes/teacher.php'));
+            Route::prefix('admin')->group(base_path('routes/admin.php'));
         },
     )
     ->withMiddleware(function (Middleware $middleware) {
        $middleware->alias([
-        'isAdmin'=>\App\Http\Middleware\IsAdmin::class,
+        'isAdmin'=>IsAdmin::class,
 
        ]);
     })
---
+++ routes/admin.php
@@ -0,0 +1,42 @@
+<?php
+use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\ClassesController;
+
+
+Route::group([
+    'controller'=>ClassesController::class,
+    'prefix'=>'classes',
+    'as'=>'classes.',
+    //apply custom middleware isAdmin + custom routes 
+    'middleware'=>['web','verified','isAdmin','auth'],
+    ],function(){
+    //create
+    Route::get('create','create')->name('create');
+    //store
+    Route::post('store','store')->name('store');
+    //index
+    Route::get('','index')->name('index');
+    //edit
+    Route::get('{id}/edit','edit')->name('edit');
+    //update
+    Route::put('{id}/update','update')->name('update');
+    //show
+    Route::get('{id}/show','show')->name('show');
+    //request delete
+    Route::delete('delete','destroy')->name('destroy');
+    //trashed
+    Route::get('trashed','showDeleted')->name('showDeleted');
+    //task7
+    //restore
+    Route::patch('{id}/restore','restore')->name('restore');
+    //forcedelete
+    Route::delete('{id}/forcedelete','forceDelete')->name('forceDelete');
+    //upload image
+    Route::get('uploadform','uploadForm');
+    Route::post('upload','upload')->name('upload');
+    
+    });
+
+
+ 
